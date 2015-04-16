13b5f5adc9f3ee9ca7e1587421921362af467c23
Anaibi <burgostatiana@yahoo.com>
2015-04-16 17:01:42 +0200

    case <pre> followed by more <pre>

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-$(function() {
+$(window).load(function() {
 
 	// fix for <pre> elements floated to right for third column layout
 	// for each <pre> element, PREVIOUS (usuallyl <p>) gets floated left 
 	// set height of both to be the same
+	// special case <pre> followed by one or more <pre> tags, 
+	// jump the each in this case also for all but the first <pre> !!
 
     $.each($('.content').find('pre'), function(i, preElement) {
       // get previous sibling of this pre element
       var $previous = $(preElement).prev();
+      
+      // if previous elements is <pre>, jump each as it's allready accounted for
+      if ($previous.prop('tagName') === 'PRE') return;
+      
       // get heights
       var pre_height = $(preElement).height();
       var previous_height = $previous.height();
 
-      // get next siblign for case <pre> is followed by one or more <pre> elements
+      // get next sibling for case <pre> is followed by one or more <pre> elements
       var $next = $(preElement).next();
-      console.log($next.prop('tagName'));
 
       // case <pre> is followed by one or more <pre>
       if ($next.prop('tagName') === 'PRE') {
-      	while ($next.prop('tagName') === 'PRE') {
-      	  pre_height = pre_height + $next.height();
+      	var i = 2; // to include padding 
+      	while ($next.prop('tagName') === 'PRE') { console.log(pre_height);
+      	  pre_height = pre_height + $next.height() + parseInt($next.css('padding-top')) * i;
           $next = $next.next();
+          i++;
         }
       }
 
