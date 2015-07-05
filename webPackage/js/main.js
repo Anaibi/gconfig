12f64eab1c0a2085d2164d661bf8a6d11fe414b5
Anaibi <burgostatiana@yahoo.com>
2015-07-05 16:18:40 +0200

    refactor main.js

--- webPackage/js/main.js
+++ webPackage/js/main.js
@@ -1,22 +1,27 @@
 $(window).load(function() {
 
   var threeColumnContentCalled = false;
-  var windowWidthMark = 1180;
+  var minWidth_threeColumn = 1180;
+  var ww = $(window).width();
 
   var preElements = $('.content').find('pre');
 
-  // fix css for three column layout 
-  threeColumnContent();
+  if (ww > minWidth_threeColumn) {
+    // fix css for three column layout 
+    threeColumnCss();
+  }
 	
-  $(window).resize(function() {
-    // fix css for three column layout
-    if (!threeColumnContentCalled) {
-	    threeColumnContent();
+  $(window).resize(function() { console.log('in resize');
+    // new window width
+    ww = $(window).width();
+    if (!threeColumnContentCalled && (ww > minWidth_threeColumn)) { 
+      threeColumnCss();
     }
-    hideScrollbarMoz();
+    //hideScrollbarMoz();
   });
 
 
+  // --------------------------------------------------------------------- hideScrollbarMoz
   // testing
   function hideScrollbarMoz() {
   	var cW, sW;
@@ -33,97 +38,72 @@ $(window).load(function() {
   	});
   };
 
-  hideScrollbarMoz();
+  //hideScrollbarMoz();
 
 
-  // fix for <pre> elements floated to right for third column layout
-  // each <pre> element gets floated right 
+  // ----------------------------------------------------------------------- threeColumnCss
+  // fix css for three column layout (fill gap)
+  // each <pre> element gets floated right and leaves 'empty' background space
   // special case <pre> followed by one or more <pre> tags, 
   // jump the each in this case for all but the first <pre> 
   //
   // want each element before <pre> tags to have total height (height + margin and padding) 
   // to be same as floated <pre> at right
-  function threeColumnContent() {
-    // if not needed, return
-  	if ($(window).width() < windowWidthMark) {
-      return;
-  	} else {
-  	  threeColumnContentCalled = true;
-  	}
+  function threeColumnCss() { 
+    
+  	threeColumnContentCalled = true;
 
     $.each(preElements, function(i, preElement) {
       
       var $this_pre = $(preElement);
-      
-      // get previous sibling of this pre element
       var $previous = $this_pre.prev();
+      var $next = $this_pre.next();
       
       // if previous elements is <pre>, jump each as it's allready accounted for
-      if (isPre($previous)) {
-      	return;
-      }
+      if (isPre($previous)) return;
       
-      // get measures
-      // total height of preElement
+      // get heights 
       var this_pre_heights = getHeights($this_pre);
-
-      // total height of previous to this preElement
       var previous_heights = getHeights($previous);
+      
 
-      // get next sibling for case <pre> is followed by one or more <pre> elements
-      var $next = $this_pre.next(); 
-
-      // calculate pre heights 
+      // calculate gap to fill 
       // top measures don't affect 
-      // if only one pre element, total height to consider if paddings are the same, inner height
-      var this_pre_height = this_pre_heights.inner_height;
-      var previous_height = previous_heights.inner_height;
+      // if only one pre element, total height to consider if paddings are the same is the inner height
+      var fillHeight = this_pre_heights.inner_height;
 
       // case <pre> is followed by one or more <pre>
       // if more then one pre element, sum of inner heights and 'inner' paddings
-      if (isPre($next.prop('nodeName'))) { 
+      if (isPre($next)) {
       	
-        this_pre_height = this_pre_height + this_pre_heights.padding_bottom;
-      	var next_pre_heights = getHeights($next);
+        // include padding between this and next element
+        fillHeight += this_pre_heights.padding_bottom;
 
-      	while (isPre($next.prop('nodeName'))) { 
-          this_pre_height = this_pre_height + next_pre_heights.inner_height + this_pre_heights.padding_top;
+      	while (isPre($next)) { 
+          fillHeight +=  $next.height() + this_pre_heights.padding_top;
           $next = $next.next();
         }
+      }
 
-        // if previous height < pre_height
-        if (previous_height < this_pre_height) { 
-          // set -margin and padding to compensate float empty space
-          $previous.css('margin-bottom', - (this_pre_height - previous_heights.inner_height));
-          $previous.css('padding-bottom', (this_pre_height - previous_heights.inner_height + previous_heights.padding_bottom));
-        }
-
-      } else {
-
-      	// if previous height < pre_height
-        if (previous_height < this_pre_height) { 
-          // set -margin and padding to compensate float empty space
-          $previous.css('margin-bottom', - (this_pre_height - previous_heights.inner_height));
-          $previous.css('padding-bottom', (this_pre_height - previous_heights.inner_height + previous_heights.padding_bottom));
-        }
-
+      if (previous_heights.inner_height < fillHeight) { 
+        // set -margin and padding to compensate float empty space
+        $previous.css('margin-bottom', -(fillHeight - previous_heights.inner_height));
+        $previous.css('padding-bottom', (fillHeight - previous_heights.inner_height + previous_heights.padding_bottom));
       }
     });
 
-  	// return true if is pre element
-  	function isPre(nodeName) { 
-  	  return ($(nodeName).prop('nodeName') === 'PRE');
+  	// return true if elem is pre element
+  	function isPre(elem) { 
+  	  return ($(elem).prop('nodeName') === 'PRE');
   	}
 
-  	// return heights of element: inner height, padding-top, padding-bottom
+  	// return heights of element
   	function getHeights(elem) { 
-  	  var h = {};
-
-  	  h.inner_height = $(elem).height();
-  	  h.padding_bottom = parseInt($(elem).css('padding-bottom'));
-  	  h.padding_top = parseInt($(elem).css('padding-top'));
-
-  	  return h;
+  	  return {
+        inner_height: $(elem).height(),
+        padding_bottom: parseInt($(elem).css('padding-bottom')),
+        padding_top: parseInt($(elem).css('padding-top'))
+      };
   	}
   }
 });
